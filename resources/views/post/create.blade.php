6acacc2bb103bcd8038b731d68721f2cd7994fe2
Виктор Иванов <v.ivaniv@agrohold.ru>
2023-03-30 16:57:37 +0300

    add model

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -2,18 +2,53 @@
 @section('content')
     <form action="{{ route('post.store') }}" method="post">
         @csrf
-    <div class="mb-3">
-        <label for="title" class="form-label">Title</label>
-        <input type="text" class="form-control" id="title" placeholder="Title" name="title">
-    </div>
-    <div class="mb-3">
-        <label for="content" class="form-label">Title</label>
-        <textarea class="form-control" id="content" placeholder="Content" name="content"></textarea>
-    </div>
-    <div class="mb-3">
-        <label for="image" class="form-label">Image.jpg</label>
-        <input type="text" class="form-control" id="image" name="image">
-    </div>
-    <button class="btn btn-primary" type="submit">Send</button>
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" placeholder="Title" name="title"
+                   value="{{old('title')}}">
+            @error('title')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Title</label>
+            <textarea class="form-control" id="content" placeholder="Content"
+                      name="content">{{old('content')}}</textarea>
+            @error('content')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="image" class="form-label">Image.jpg</label>
+            <input type="text" class="form-control" value="{{old('image')}}" id="image" name="image">
+            @error('image')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="category">Category</label>
+            <select class="form-select" id="category" name="category_id">
+                @foreach($categories as $category)
+                    <option
+                        {{ old('category_id')==$category->id? 'selected':'' }}
+                        value="{{$category->id}}">{{$category->title}}</option>
+
+                @endforeach
+            </select>
+        </div>
+
+        @foreach($tags as $tag)
+
+            <div class="form-check mt-3 mb-3">
+                <input class="form-check-input" type="checkbox"
+                       value="{{$tag->id}}" id="tags{{$tag->id}}"
+                       name="tags[]">
+                <label class="form-check-label" for="tags{{$tag->id}}">
+                    {{$tag->title}}
+                </label>
+            </div>
+        @endforeach
+
+        <button class="btn btn-primary" type="submit">Send</button>
     </form>
 @endsection
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.main')
 @section('content')
     <form action="{{ route('post.update', $post->id) }}" method="post">
@@ -6,16 +5,40 @@
         @method('patch')
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" placeholder="Title" name="title" value="{{$post->title}}">
+            <input type="text" class="form-control" id="title" placeholder="Title" name="title"
+                   value="{{$post->title}}">
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Title</label>
-            <textarea class="form-control" id="content" placeholder="Content" name="content">{{$post->content}}</textarea>
+            <textarea class="form-control" id="content" placeholder="Content"
+                      name="content">{{$post->content}}</textarea>
         </div>
         <div class="mb-3">
             <label for="image" class="form-label">Image.jpg</label>
             <input type="text" class="form-control" id="image" name="image" value="{{$post->image}}">
-        </div>
-        <button class="btn btn-primary" type="submit">Update</button>
+
+            <div class="mb-3 mt-3">
+                <label for="category">Category</label>
+                <select class="form-select" id="category" name="category_id">
+                    @foreach($categories as $category)
+                        <option
+                            {{ $category->id===$post->category->id ? ' selected':''}}
+                            value="{{$category->id}}">{{$category->title}}</option>
+                    @endforeach
+                </select>
+            </div>
+            @foreach($tags as $tag)
+                <div class="form-check mt-3 mb-3">
+                    <input class="form-check-input" type="checkbox"
+                           @foreach($post->tags as $postTag)
+                               {{ $tag->id===$postTag->id ? ' checked':''}}
+                           @endforeach
+                           value="{{$tag->id}}" id="tags{{$tag->id}}" name="tags[]">
+                    <label class="form-check-label" for="tags{{$tag->id}}">
+                        {{$tag->title}}
+                    </label>
+                </div>
+            @endforeach
+            <button class="btn btn-primary" type="submit">Update</button>
     </form>
 @endsection
