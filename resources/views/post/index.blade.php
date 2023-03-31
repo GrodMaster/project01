58f47b7f1d97388eafcfe52a615467b8c8bd7cb7
Виктор Иванов <v.ivaniv@agrohold.ru>
2023-03-31 10:53:46 +0300

    add filter and services

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.main')
 @section('content')
-    <div ><a href="{{route('post.create')}}" class="btn btn-primary m-3">Add</a></div>
+    <div><a href="{{route('post.create')}}" class="btn btn-primary m-3">Add</a></div>
     <div>
         @foreach($posts as $post)
             <div class="m-3"><a href="{{route('post.show', $post->id)}}">{{$post->id}}. {{$post->title}}</a></div>
+{{--            <div class="m-3"><img src="{{$post->image}}" width="200px"></div>--}}
+{{--            <div class="m-3">{{$post->content}}</div>--}}
         @endforeach
+        <div class="mt-5">
+            {{$posts->links()}}
+        </div>
     </div>
 @endsection
